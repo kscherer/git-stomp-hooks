b1b48920eb51b9f4704ceb9f2721ec071720f638
Konrad Scherer <Konrad.Scherer@windriver.com>
2012-08-20 14:22:08 -0400

    Drop root privs

--- git-stomp-listener.py
+++ git-stomp-listener.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os, sys
+import pwd, grp
 from stomplistenerdaemon import StompListenerDaemon
 
 #stomp uses logging module
@@ -19,7 +20,27 @@ logger.addHandler(filehandler)
 #help python find the stomp module packaged with the script
 os.chdir(os.path.dirname(sys.argv[0]))
 
+def drop_privileges(uid_name='nobody', gid_name='nogroup'):
+    if os.getuid() != 0:
+        # We're not root so, like, whatever dude
+        return
+
+    # Get the uid/gid from the name
+    running_uid = pwd.getpwnam(uid_name).pw_uid
+    running_gid = grp.getgrnam(gid_name).gr_gid
+
+    # Remove group privileges
+    os.setgroups([])
+
+    # Try setting the new uid/gid
+    os.setgid(running_gid)
+    os.setuid(running_uid)
+
+    # Ensure a very conservative umask
+    old_umask = os.umask(077)
+
 if __name__ == "__main__":
+    drop_privileges('puppet')
     daemon = StompListenerDaemon('/var/tmp/stomp-listener.pid')
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
